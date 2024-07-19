91c627a88141bb89da0006a2a9c71123ba9101ec
AnhTCQ2785 <truongcaoquocanh@gmail.com>
2024-07-19 13:09:53 +0000

    19/7/2024 - update day12

--- day12/Practice 1.py
+++ day12/Practice 1.py
@@ -2,13 +2,16 @@ def add(a, b):
     result = a + b
     print(f"The result of adding {a} and {b} is {result}")
 
+
 def subtract(a, b):
     result = a - b
     print(f"The result of subtracting {b} from {a} is {result}")
 
+
 def multiply(a, b):
     result = a * b
     print(f"The result of multiplying {a} and {b} is {result}")
+
 
 def divide(a, b):
     if b == 0:
