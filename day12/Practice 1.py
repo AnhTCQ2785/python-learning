68f3a8202adc3e300f76fd9e90ad589be4d6ebe7
AnhTCQ2785 <truongcaoquocanh@gmail.com>
2024-07-19 13:22:59 +0000

    update code

--- day12/Practice 1.py
+++ day12/Practice 1.py
@@ -1,21 +1,23 @@
 def add(a, b):
-    result = a + b
-    print(f"The result of adding {a} and {b} is {result}")
+    print(f"The result of adding {a} and {b} is {a + b}")
 
 
 def subtract(a, b):
-    result = a - b
-    print(f"The result of subtracting {b} from {a} is {result}")
+    print(f"The result of subtracting {b} from {a} is {a-b}")
 
 
 def multiply(a, b):
-    result = a * b
-    print(f"The result of multiplying {a} and {b} is {result}")
+    print(f"The result of multiplying {a} and {b} is {a * b}")
 
 
 def divide(a, b):
     if b == 0:
         print("Warning: Division by zero is not allowed.")
     else:
-        result = a / b
-        print(f"The result of dividing {a} by {b} is {result}")
+        print(f"The result of dividing {a} by {b} is {a / b}")
+
+
+add(3, 4)
+subtract(6, 5)
+multiply(7, 8)
+divide(8, 4)
